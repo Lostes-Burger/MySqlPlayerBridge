575b25fe66192dd4db55bc21b46d3f37118cfb6c
Lostes_Burger <accounts@burgerbringstaft.de>
2026-02-17 10:29:16 +0100

    (3.7.2) Fixed armor disappearing on apply. (Rolled back to old usage)

--- src/main/java/de/lostesburger/mySqlPlayerBridge/Managers/SyncModules/Inventory/InventoryDataManager.java
+++ src/main/java/de/lostesburger/mySqlPlayerBridge/Managers/SyncModules/Inventory/InventoryDataManager.java
@@ -7,7 +7,6 @@ import de.lostesburger.mySqlPlayerBridge.Exceptions.NBTSerializationException;
 import de.lostesburger.mySqlPlayerBridge.Handlers.Errors.MySqlErrorHandler;
 import de.lostesburger.mySqlPlayerBridge.Main;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -62,12 +61,7 @@ public class InventoryDataManager {
             if(Main.nbtSerializer == null){
                 throw new NBTSerializationException("nbtserializer not loaded on serialize", null);
             }
-            ItemStack[] storage = player.getInventory().getContents();
-            int storageSize = Math.min(storage.length, 36);
-            ItemStack[] withOffhand = new ItemStack[storageSize + 1];
-            System.arraycopy(storage, 0, withOffhand, 0, storageSize);
-            withOffhand[withOffhand.length - 1] = player.getInventory().getItemInOffHand();
-            serializedInventory = Main.nbtSerializer.serialize(withOffhand);
+            serializedInventory = Main.nbtSerializer.serialize(player.getInventory().getContents());
             if(Main.DEBUG){ System.out.println("Inv: "+serializedInventory); }
 
         } catch (Exception e) {
@@ -118,14 +112,7 @@ public class InventoryDataManager {
                     if(Main.nbtSerializer == null){
                         throw new NBTSerializationException("nbtserializer not loaded", null);
                     }
-                    ItemStack[] data = Main.nbtSerializer.deserialize(String.valueOf(entry.get("inventory")));
-                    ItemStack[] storage = new ItemStack[36];
-                    int copyLen = data.length >= 37 ? 36 : Math.min(36, data.length);
-                    System.arraycopy(data, 0, storage, 0, copyLen);
-                    player.getInventory().setContents(storage);
-                    if(data.length >= 37){
-                        player.getInventory().setItemInOffHand(data[data.length - 1]);
-                    }
+                    player.getInventory().setContents(Main.nbtSerializer.deserialize(String.valueOf(entry.get("inventory"))));
                 } catch (Exception e) {
                     MySqlErrorHandler errorHandler = new MySqlErrorHandler();
                     String errorId = errorHandler.logSyncError("Inventory", "deserialize", Main.TABLE_NAME_INVENTORY, player,
