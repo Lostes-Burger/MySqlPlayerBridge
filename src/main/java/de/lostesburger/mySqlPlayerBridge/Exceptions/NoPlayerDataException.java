f63fbdb3c4149c79e7a4acb7c123c8ee40f55860
Lostes_Burger <accounts@burgerbringstaft.de>
2026-05-20 12:04:07 +0200

    (3.7.5) Removed unused Exception

--- src/main/java/de/lostesburger/mySqlPlayerBridge/Exceptions/NoPlayerDataException.java
+++
@@ -1,9 +0,0 @@
-package de.lostesburger.mySqlPlayerBridge.Exceptions;
-
-import org.bukkit.entity.Player;
-
-public class NoPlayerDataException extends Exception{
-    public NoPlayerDataException(Player player){
-        super("No player data found for player: "+player.getName());
-    }
-}
